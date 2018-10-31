76ce63ef31f629daab99c3c2545f8e4ac57d911c
Shane Loretz <sloretz@osrfoundation.org>
2018-10-31 15:49:28 -0700

    C typesupport returns rcl_action_type_support_t

--- test_msgs/include/test_msgs/action/fibonacci.h
+++ test_msgs/include/test_msgs/action/fibonacci.h
@@ -1,16 +1,5 @@
-// Copyright 2018 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// generated from rosidl_generator_c/resource/msg__type_support.h.em
+// generated code does not contain a copyright notice
 
 #ifndef TEST_MSGS__ACTION__FIBONACCI_H_
 #define TEST_MSGS__ACTION__FIBONACCI_H_
@@ -20,53 +9,13 @@ extern "C"
 {
 #endif
 
-#include "rosidl_typesupport_interface/macros.h"
 #include <action_msgs/msg/goal_info.h>
 #include <action_msgs/msg/goal_status_array.h>
 #include <action_msgs/srv/cancel_goal.h>
-#include "rosidl_generator_c/message_type_support_struct.h"
-#include "rosidl_generator_c/service_type_support_struct.h"
+#include <test_msgs/action/fibonacci__feedback.h>
 #include <test_msgs/action/fibonacci__goal.h>
 #include <test_msgs/action/fibonacci__result.h>
-#include <test_msgs/action/fibonacci__feedback.h>
-
-
-// TODO(sloretz) define these macros in rosidl_typesupport_interface,
-// or rosidl_generator_c, or rosidl_type_support_c
-
-#define ROSIDL_GET_ACTION_GOAL_REQUEST(package, action) \
-  ROSIDL_TYPESUPPORT_INTERFACE__ ##  package ## __ACTION__ ## action ## __GOAL_REQUEST_SYMBOL_NAME
-
-#define ROSIDL_GET_ACTION_GOAL_RESPONSE(package, action) \
-  ROSIDL_TYPESUPPORT_INTERFACE__ ##  package ## __ACTION__ ## action ## __GOAL_RESPONSE_SYMBOL_NAME
-
-#define ROSIDL_GET_ACTION_FEEDBACK(package, action) \
-  ROSIDL_TYPESUPPORT_INTERFACE__ ##  package ## __ACTION__ ## action ## __FEEDBACK_SYMBOL_NAME
-
-#define ROSIDL_GET_ACTION_STATUS(package, action) \
-  ROSIDL_TYPESUPPORT_INTERFACE__ ##  package ## __ACTION__ ## action ## __STATUS_SYMBOL_NAME
-
-#define ROSIDL_GET_ACTION_CANCEL_REQUEST(package, action) \
-  ROSIDL_TYPESUPPORT_INTERFACE__ ##  package ## __ACTION__ ## action ## __CANCEL_REQUEST_SYMBOL_NAME
-
-// TODO(sloretz) end todo
-
-
-#define ROSIDL_TYPESUPPORT_INTERFACE__TEST_MSGS__ACTION__FIBONACCI__GOAL_REQUEST_SYMBOL_NAME \
-  ROSIDL_TYPESUPPORT_INTERFACE__SERVICE_SYMBOL_NAME(rosidl_typesupport_c, test_msgs, action, Fibonacci_Goal)
-
-#define ROSIDL_TYPESUPPORT_INTERFACE__TEST_MSGS__ACTION__FIBONACCI__GOAL_RESPONSE_SYMBOL_NAME \
-  ROSIDL_TYPESUPPORT_INTERFACE__SERVICE_SYMBOL_NAME(rosidl_typesupport_c, test_msgs, action, Fibonacci_Result)
-
-#define ROSIDL_TYPESUPPORT_INTERFACE__TEST_MSGS__ACTION__FIBONACCI__FEEDBACK_SYMBOL_NAME \
-  ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_c, test_msgs, action, Fibonacci_Feedback)
-
-// TODO(sloretz) not sure these are needed since it's always the same symbol
-#define ROSIDL_TYPESUPPORT_INTERFACE__TEST_MSGS__ACTION__FIBONACCI__STATUS_SYMBOL_NAME \
-  ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_c, action_msgs, msg, GoalStatusArray)
-
-#define ROSIDL_TYPESUPPORT_INTERFACE__TEST_MSGS__ACTION__FIBONACCI__CANCEL_REQUEST_SYMBOL_NAME \
-  ROSIDL_TYPESUPPORT_INTERFACE__SERVICE_SYMBOL_NAME(rosidl_typesupport_c, action_msgs, srv, CancelGoal)
+#include "test_msgs/action/fibonacci__type_support.h"
 
 #ifdef __cplusplus
 }
---
+++ test_msgs/include/test_msgs/action/fibonacci__type_support.h
@@ -0,0 +1,26 @@
+// generated from rosidl_generator_c/resource/msg__type_support.h.em
+// generated code does not contain a copyright notice
+
+#ifndef TEST_MSGS__ACTION__FIBONACCI__TYPE_SUPPORT_H_
+#define TEST_MSGS__ACTION__FIBONACCI__TYPE_SUPPORT_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "rosidl_generator_c/action_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+
+#include "test_msgs/msg/rosidl_generator_c__visibility_control.h"
+
+// Forward declare the get type support functions for this type.
+ROSIDL_GENERATOR_C_PUBLIC_test_msgs
+const rosidl_action_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__ACTION_SYMBOL_NAME(rosidl_typesupport_c, test_msgs, action, Fibonacci)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // TEST_MSGS__ACTION__FIBONACCI__TYPE_SUPPORT_H_
