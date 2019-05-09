0aba5a142878c2077d7a03977087e7d74d40ee68
Michel Hidalgo <michel@ekumenlabs.com>
2019-05-09 18:05:03 -0300

    Add UTF-8 BOM to message_fixtures.hpp file (#79)
    * Add UTF-8 BOM to message_fixtures.hpp file
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * add cpplint ignore / note about the BOM for visibility
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- test_msgs/include/test_msgs/message_fixtures.hpp
+++ test_msgs/include/test_msgs/message_fixtures.hpp
@@ -1,3 +1,5 @@
+﻿// NOLINT: This file starts with a BOM since it contain non-ASCII characters
+//
 // Copyright 2015 Open Source Robotics Foundation, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
